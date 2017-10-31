6d39729cbcebf2fc186592fcb80bcb0a00b671fc
Yujiro Saito <yujiro@Yujiro-Saito-no-MacBook-Air.local>
2017-11-01 00:20:02 +0900

    学習モデルの読み込みと学習結果表示

--- MachineLearning/ViewController.swift
+++ MachineLearning/ViewController.swift
@@ -7,19 +7,24 @@
 //
 
 import UIKit
+import CoreML
 
-class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePickerControllerDelegate {
+
+class ViewController: UIViewController,UINavigationControllerDelegate {
 
     // MARK: - Property
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var resultLabel: UILabel!
-    let picker = UIImagePickerController()
+    var model: Inceptionv3!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        model = Inceptionv3()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        picker.delegate = self
-        picker.allowsEditing = false
     }
 
     // MARK: - Method
@@ -29,18 +34,81 @@ class ViewController: UIViewController,UINavigationControllerDelegate,UIImagePic
             return
         }
         
-        picker.sourceType = .camera
-        present(picker,animated: true)
+        let cameraPicker = UIImagePickerController()
+        cameraPicker.delegate = self
+        cameraPicker.sourceType = .camera
+        cameraPicker.allowsEditing = false
+        
+        present(cameraPicker, animated: true)
+        
+        
     }
     
     @IBAction func libraryButtonDidTap(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = false
+        picker.delegate = self
         picker.sourceType = .photoLibrary
         present(picker, animated: true)
     }
     
+    
+    
+}
+
+extension ViewController: UIImagePickerControllerDelegate {
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true)
+        resultLabel.text = "画像解析中"
+        guard let image = info["UIImagePickerControllerOriginalImage"] as? UIImage else {
+            return
+        }
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: 299, height: 299), true, 2.0)
+        image.draw(in: CGRect(x: 0, y: 0, width: 299, height: 299))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        
+        let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue, kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+        var pixelBuffer : CVPixelBuffer?
+        let status = CVPixelBufferCreate(kCFAllocatorDefault, Int(newImage.size.width), Int(newImage.size.height), kCVPixelFormatType_32ARGB, attrs, &pixelBuffer)
+        guard (status == kCVReturnSuccess) else {
+            return
+        }
+        
+        CVPixelBufferLockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer!)
+        
+        let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+        let context = CGContext(data: pixelData, width: Int(newImage.size.width), height: Int(newImage.size.height), bitsPerComponent: 8, bytesPerRow: CVPixelBufferGetBytesPerRow(pixelBuffer!), space: rgbColorSpace, bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) 
+        
+        context?.translateBy(x: 0, y: newImage.size.height)
+        context?.scaleBy(x: 1.0, y: -1.0)
+        
+        UIGraphicsPushContext(context!)
+        newImage.draw(in: CGRect(x: 0, y: 0, width: newImage.size.width, height: newImage.size.height))
+        UIGraphicsPopContext()
+        CVPixelBufferUnlockBaseAddress(pixelBuffer!, CVPixelBufferLockFlags(rawValue: 0))
+        photoImageView.image = newImage
+        
+        guard let prediction = try? model.prediction(image: pixelBuffer!) else {
+            return
+        }
+        
+        resultLabel.text = "この画像は\(prediction.classLabel)ですか？"
+        
+    }
+    
+    
+    
+    
+    
     
     
     
